761d8a4d05bd9368286a5c08ac53868cab92d90f
Giorgio <giorgio.diprima@edu.turkuamk.fi>
2021-01-22 16:34:06 +0200

    Insert in a list
    to fix the sorting

--- app.py
+++ app.py
@@ -1,24 +1,58 @@
 from flask import Flask
 from math import cos, asin, sqrt, pi
 import json
+from functools import cmp_to_key
+from resturant import Resturant
 app = Flask(__name__)
 #export FLASK_ENV=development
 #export FLASK_APP=app.py
 # flask run
+restaurants = []
 
+#TODO calculate the listo fresturant just the first time 
 @app.route('/')
 def comp():
-#import all object from json and create the list
+    latUsr = 60.1709
+    lonUsr = 24.941 
+
+
+    #----------------------------------------populating list
+   # global restaurants
     with open("restaurants.json") as jsonFile:
         jsonResturants = json.load(jsonFile)        #return a dictionary
         jsonFile.close()
 
     # Iterating through the json 
-    # list 
-    myPrint = ""
-    for i in jsonResturants["restaurants"]: 
-        myPrint += i["name"] + "\n" 
-    return myPrint
+    # list
+    for jsonResturant in jsonResturants["restaurants"]:
+        location = jsonResturant["location"]
+        objResturant =  Resturant(lat =  location[0], lon = location[1], online = jsonResturant['online'], popularity = jsonResturant['popularity'], launchDate = jsonResturant['launch_date'], name = jsonResturant['name'] )
+
+        restaurants.append((objResturant, objResturant.distance(latUsr, lonUsr)))
+        ##restaurants.append((objResturant, 3))
+        
+    #----------------------------------------end 
+
+    
+
+    # or i can use sorted if i want to return the sorted list, without changing the original
+    # most popular first
+    restaurants.sort(key=(lambda resturant: resturant[0].popularity ), reverse = True)
+    # for resturant in restaurants:
+    #     if resturant.dis
+    #newest first
+    restaurants.sort(key=(lambda resturant: resturant[0].launchDate), reverse = True)
+    #nearby first
+    restaurants.sort(key= (lambda resturant : resturant[1] ), reverse = True)
+
+    
+
+    str = ""
+    for resturant in restaurants:
+        str += resturant[0].__str__()
+    return str
+#import all object from json and create the list
+    
 
 if __name__ == '__main__':
     app.run()
--- resturant.py
+++ resturant.py
@@ -1,19 +1,25 @@
+from datetime import date
+
 class Resturant:
 
     distance = -1
-    def __init__(self, lat, lon, online, populaty, openingDate):
+    def __init__(self, lat, lon, online, popularity, launchDate, name):
         self.lat = lat
         self.lon = lon
         self.online = online
-        self.populaty = populaty
-        self.openingDate = openingDate
+        self.popularity = popularity
+        self.launchDate = date.fromisoformat(launchDate)
+        self.name = name
         
 
-    def distance(self, lat, lon):
-        distnce = foodistance(self.lat, self.lon, lat, lon)
-
-    def foodistance(lat1, lon1, lat2, lon2):
+    def prova(lat1, lon1, lat2, lon2):
         p = pi/180
         a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
         return 12742 * asin(sqrt(a)) #2*R*asin...
     
+    def distance(self, lat, lon):
+        return prova(self.lat, self.lon, lat, lon)
+
+    
+    def __str__(self):
+        return "\n\nI'm resturant {name} situated in {lat}, {lon} I'm {popularity:.2f} popular I'm open since {launchDate} and now I'm online = {online}!\n".format(lat = self.lat, lon = self.lon, online = self.online, popularity = self.popularity, launchDate = self.launchDate, name = self.name )
